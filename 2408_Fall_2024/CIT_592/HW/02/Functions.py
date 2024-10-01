aaa833415a2c8b07837fe6094e239f211ef161c5
hozy galbook4 <danielisdan@naver.com>
2024-10-01 18:31:27 -0400

    241001 covered with galbook4 :

--- 2408_Fall_2024/CIT_592/HW/02/Functions.py
+++ 2408_Fall_2024/CIT_592/HW/02/Functions.py
@@ -16,10 +16,8 @@ if __name__ == '__main__':
     # x = factorial(23) / factorial(10) / factorial(6) / factorial(4) / factorial(3)
     # print(x)
 
-    a = factorial(23) / factorial(10) / factorial(6) / factorial(4) / factorial(3)
-    print(a)
+    a = comb(18,3) -comb(17,2)
 
-    b = factorial(13) * factorial(9) * factorial(7) / factorial(10) / factorial(6) / factorial(4)  / math.pow(factorial(3), 3)
-    print(b)
+    b = comb(17, 3)
 
     print(a-b)
