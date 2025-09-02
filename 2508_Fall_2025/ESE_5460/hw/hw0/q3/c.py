0c79760c42649058891af4cbae0025d714106011
hozy <hozy5333@gmail.com>
2025-09-01 22:47:01 -0400

    250901 covered with galbook4 :

--- 2508_Fall_2025/ESE_5460/hw/hw0/q3/c.py
+++ 2508_Fall_2025/ESE_5460/hw/hw0/q3/c.py
@@ -4,13 +4,20 @@ from scipy.optimize import minimize
 # Objective function    
 def f(z):
     x, y = z
-    return x**2+y**2-6*x*y-4*x-5*y
+    return x**2 + y**2 - 6*x*y - 4*x - 5*y
 
 # Constraints    
 # y <= -(x-2)^2+4
-cons1 = {'type':'ineq','fun':lambda z: -(z[1] - (-(z[0]-2)**2+4))} 
+cons1 = {'type':'ineq','fun':lambda z: 4 -(z[0]-2)**2 - z[1]} 
 # y >= -x+1
-cons2 = {'type':'ineq','fun':lambda z: z[1] + z[0] -1} 
+cons2 = {'type':'ineq','fun':lambda z: z[0] + z[1] - 1} 
 
-res = minimize(f,[0,0],constraints=[cons1,cons2])
-print(res.x,res.fun)
+initial_point = [1, 3]
+
+res = minimize(f, initial_point, constraints=[cons1,cons2], method='SLSQP')
+print(f'Optimal Point : {res.x}, Value : {res.fun}')
+
+cons1_changed = {'type':'ineq','fun':lambda z: 4.1 -(z[0]-2)**2 - z[1]} 
+
+res = minimize(f, initial_point,constraints=[cons1_changed,cons2], method='SLSQP')
+print(f'Optimal Point : {res.x}, Value : {res.fun}')
